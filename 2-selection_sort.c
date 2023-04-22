2c71ef96847c81f2eeb3140e3015e852f5610679
NewGit1999 <yasminebenali32@gmail.com>
2023-04-22 16:29:09 -0700

    select algorithm in array

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,7 +7,7 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t a, less, b = 0;
-	int select = 0;
+	int selection = 0;
 
 	if (size == 0 || size == 1)
 		return;
@@ -23,9 +23,9 @@ void selection_sort(int *array, size_t size)
 		}
 		if (b != less)
 		{
-			select = array[b];
+			selection = array[b];
 			array[b] = array[less];
-			array[less] = select;
+			array[less] = selection;
 			print_array(array, size);
 		}
 		b++;
