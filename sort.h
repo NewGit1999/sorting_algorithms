71f5400acb714ef09b6ae4b07d3c6dcf2e056622
NewGit1999 <yasminebenali32@gmail.com>
2023-04-23 15:22:33 -0700

    update header file

--- sort.h
+++ sort.h
@@ -23,6 +23,9 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap(int *x, int *y);
+int lomuto_partition(int *array, size_t size, int el1, int el2);
+void sortpart(int *array, int el1, int el2, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 
